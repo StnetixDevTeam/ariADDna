4e03c7b6825102e56fab7be7024c17ff41fbef64
slonikmak <slonikmak@gmail.com>
2017-01-29 17:27:44 +0300

    revision gui configuration class

--- desktop-gui/src/main/java/com/stnetix/ariaddna/desktopgui/configs/GuiConfig.java
+++ desktop-gui/src/main/java/com/stnetix/ariaddna/desktopgui/configs/GuiConfig.java
@@ -1,9 +1,7 @@
 package com.stnetix.ariaddna.desktopgui.configs;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.stnetix.ariaddna.desktopgui.controllers")
 public class GuiConfig {
 }
