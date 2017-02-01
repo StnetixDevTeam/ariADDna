2f1a0793486fffd18c8719d1bd2454bb6c390c7c
Alexandr Kotov <a200x@bk.ru>
2017-02-01 12:13:36 +0300

    Add constructors and method getErrorMessage to AriaddnaExeption class

--- common-utils/src/main/java/com/stnetix/ariaddna/commonutils/exeption/AriaddnaExeption.java
+++ common-utils/src/main/java/com/stnetix/ariaddna/commonutils/exeption/AriaddnaExeption.java
@@ -3,6 +3,62 @@ package com.stnetix.ariaddna.commonutils.exeption;
 /**
  * This class is the parent of all exceptions in the Ariaddna project.
  */
+
 public class AriaddnaExeption extends Exception {
 
+    private String errorMessage;
+
+    /**
+     * Constructor of new AriaddnaExeption with the specified cause.
+     * @param cause
+     * */
+    public AriaddnaExeption(Throwable cause) {
+        super(cause);
+    }
+
+    /**
+     * Constructor of new AriaddnaExeption with traceMessage.
+     * @param traceMessage
+     * */
+    public AriaddnaExeption(String traceMessage) {
+        super(traceMessage);
+    }
+
+    /**
+     * Constructor of new AriaddnaExeption with traceMessage and errorMessage.
+     * @param traceMessage
+     * @param errorMessage
+     * */
+    public AriaddnaExeption(String traceMessage, String errorMessage){
+        super(traceMessage);
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * Constructor of new AriaddnaExeption with traceMessage and the specified cause.
+     * @param traceMessage
+     * @param cause
+     * */
+    public AriaddnaExeption(String traceMessage, Throwable cause) {
+        super(traceMessage, cause);
+    }
+
+    /**
+     * Constructor of new AriaddnaExeption with traceMessage, errorMessage and the specified cause.
+     * @param traceMessage
+     * @param errorMessage
+     * @param cause
+     * */
+    public AriaddnaExeption(String traceMessage, String errorMessage, Throwable cause) {
+        super(traceMessage, cause);
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * Method return specific errorMessage
+     * @return errorMessage
+     * */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
 }
