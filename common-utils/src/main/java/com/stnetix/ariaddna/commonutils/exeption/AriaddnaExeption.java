fb4cac31f2d4af28362037e855531377efc98631
Alexandr Kotov <a200x@bk.ru>
2017-02-01 11:19:43 +0300

    Add common exeption classs

---
+++ common-utils/src/main/java/com/stnetix/ariaddna/commonutils/exeption/AriaddnaExeption.java
@@ -0,0 +1,8 @@
+package com.stnetix.ariaddna.commonutils.exeption;
+
+/**
+ * This class is the parent of all exceptions in the Ariaddna project.
+ */
+public class AriaddnaExeption extends Exception {
+
+}
