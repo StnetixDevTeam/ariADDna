88eb4fd7fd576e607bb123cb8443993a47d741b5
slonikmak <slonikmak@gmail.com>
2017-02-03 17:40:21 +0300

    add switching between treeViewPane and mainLeftPane

--- desktop-gui/src/main/java/com/stnetix/ariaddna/desktopgui/controllers/TreeViewLeftPaneController.java
+++ desktop-gui/src/main/java/com/stnetix/ariaddna/desktopgui/controllers/TreeViewLeftPaneController.java
@@ -1,7 +1,31 @@
 package com.stnetix.ariaddna.desktopgui.controllers;
 
+import com.stnetix.ariaddna.desktopgui.views.FXMLLoaderProvider;
+import com.stnetix.ariaddna.desktopgui.views.ViewsFactory;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 public class TreeViewLeftPaneController implements IGuiController {
+    MainController mainController;
+    FXMLLoaderProvider provider;
+
+    @FXML
+    void hidePane(ActionEvent event) throws IOException {
+        mainController.setLeftBorderContent(ViewsFactory.LEFT_MAIN.getNode(provider));
+    }
+
+    @Autowired
+    public void setMainController(MainController mainController) {
+        this.mainController = mainController;
+    }
+
+    @Autowired
+    public void setProvider(FXMLLoaderProvider provider) {
+        this.provider = provider;
+    }
 }
--- desktop-gui/src/main/java/com/stnetix/ariaddna/desktopgui/views/ViewsFactory.java
+++ desktop-gui/src/main/java/com/stnetix/ariaddna/desktopgui/views/ViewsFactory.java
@@ -5,7 +5,8 @@ import javafx.scene.Node;
 import java.io.IOException;
 
 public enum ViewsFactory {
-    MAIN("/com/stentix/ariaddna/desktopgui/fxmlViews/main.fxml"), LEFT_TREE("/com/stentix/ariaddna/desktopgui/fxmlViews/treeViewLeftPane.fxml");
+    MAIN("/com/stentix/ariaddna/desktopgui/fxmlViews/main.fxml"), LEFT_TREE("/com/stentix/ariaddna/desktopgui/fxmlViews/treeViewLeftPane.fxml"),
+    LEFT_MAIN("/com/stentix/ariaddna/desktopgui/fxmlViews/mainLeftPane.fxml");
 
     public String path;
 
