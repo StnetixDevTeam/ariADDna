4bf83f6868f1f1efd52a360d8126e5cacd0c02ac
Michael Latushko <ancord.m@yandex.ru>
2017-07-12 17:44:41 +0300

    YandexDisk draft: added Annotations to tests

--- storage-service/src/test/java/com/stnetix/ariaddna/persistence/services/CloudCredentialsServiceImplTest.java
+++ storage-service/src/test/java/com/stnetix/ariaddna/persistence/services/CloudCredentialsServiceImplTest.java
@@ -5,11 +5,19 @@ import com.stnetix.ariaddna.persistence.utils.AppConfiguration;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.UUID;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = AppConfiguration.class)
+//Transactional mark this test to rollback all changes after test
+@Transactional
 public class CloudCredentialsServiceImplTest {
     private ConfigurableApplicationContext context;
     private CloudCredentialsServiceImpl cloudCredentialsService;
