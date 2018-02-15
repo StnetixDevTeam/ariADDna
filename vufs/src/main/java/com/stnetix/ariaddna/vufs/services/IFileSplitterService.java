d51ab175c530a32387bd17faa178e6c03f3cbd58
vasap87 <a200x@bk.ru>
2018-02-15 16:24:18 +0300

    remove not used classes

--- vufs/src/main/java/com/stnetix/ariaddna/vufs/services/IFileSplitterService.java
+++
@@ -1,8 +0,0 @@
-package com.stnetix.ariaddna.vufs.services;
-
-/**
- * This interface define the methods which work with file and chunk files.
- */
-public interface IFileSplitterService {
-    //Todo write split and merge file methods
-}
