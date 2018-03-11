ecdc2063014a15548be89fe6decb229566521d9e
Vladislav Gasanov <kosmoflyko@gmail.com>
2018-03-12 02:03:25 +0300

    fix checkstyle in server-core

--- server-core/src/main/java/com/stnetix/ariaddna/servercore/configuration/ServerCoreConfiguration.java
+++ server-core/src/main/java/com/stnetix/ariaddna/servercore/configuration/ServerCoreConfiguration.java
@@ -1,12 +1,24 @@
+/*
+ * Copyright (c) 2018 stnetix.com. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package com.stnetix.ariaddna.servercore.configuration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
 
 import com.stnetix.ariaddna.pushnotification.server.PushServer;
 import com.stnetix.ariaddna.restapiserver.Swagger2SpringBoot;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.context.annotation.Import;
 
 /**
  * Created by alexkotov on 18.07.17.
@@ -14,7 +26,11 @@ import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(Swagger2SpringBoot.class)
-public class ServerCoreConfiguration  {
+public class ServerCoreConfiguration {
+
+    private ServerCoreConfiguration() {
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Swagger2SpringBoot.class, args);
         PushServer pushServer = new PushServer(8081);
