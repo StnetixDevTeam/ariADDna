f3be3252ac3cbc152cc9c80743789d797915b27f
Alexandr Kotov <a200x@bk.ru>
2018-03-26 15:14:59 +0300

    Fix code style common-utils

--- common-utils/src/main/java/com/stnetix/ariaddna/commonutils/settings/Settings.java
+++ common-utils/src/main/java/com/stnetix/ariaddna/commonutils/settings/Settings.java
@@ -1,9 +1,24 @@
+/*
+ * Copyright (c) 2018 stnetix.com. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License.  You may obtain a copy of
+ * the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed
+ * under the License is distributed on an "AS IS" BASIS, without warranties or
+ * conditions of any kind, EITHER EXPRESS OR IMPLIED.  See the License for the
+ * specific language governing permissions and limitations under the License.
+ */
+
 package com.stnetix.ariaddna.commonutils.settings;
 
 /**
  * Created by LugovoyAV on 22.02.2018.
  */
 public final class Settings {
-    public  final static String fileNameSettings = "settings.xml";
+    public static final String fileNameSettings = "settings.xml";
 
+    private Settings() {
+    }
 }
